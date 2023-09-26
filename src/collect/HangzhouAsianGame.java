3fcee732d62c2a2e9be6dcda8160a8bf422a2b4e
dkdiek <144099638+dkdiek@users.noreply.github.com>
2023-09-26 15:41:25 +0900

    230926 15:41

--- src/collect/HangzhouAsianGame.java
+++ src/collect/HangzhouAsianGame.java
@@ -8,62 +8,76 @@ import java.util.Map;
 import java.util.Set;
 
 public class HangzhouAsianGame {
-
+	
 	List eventList;
-
+	
 	public HangzhouAsianGame() {
 		eventList = new ArrayList();
 	}
-
+	
 	void setESports() {
-
+		List list = new ArrayList();
+		
 		Map map = new HashMap();
 		map.put("date", "9월 24일 (일)");
 		map.put("time", "10:00");
 		map.put("title", "FIFA 온라인");
-
-		List list = new ArrayList();
 		list.add(map);
-
+		
+		map = new HashMap();
+		map.put("date", "9월 26일 (화)");
+		map.put("time", "10:00");
+		map.put("title", "롤");
+		list.add(map);
+		
 		Map eMap = new HashMap();
 		eMap.put("eSports", list);
-
+		
 		eventList.add(eMap);
-
+		
 	}
-
+	
 	void setCycle() {
-
+		
 		Map map = new HashMap();
 		map.put("date", "10월 1일 (일)");
 		map.put("time", "11:00");
 		map.put("title", "남자 예선 1라운드");
-
+		
 		List list = new ArrayList();
 		list.add(map);
-
+		
 		Map eMap = new HashMap();
 		eMap.put("cycle", list);
-
+		
 		eventList.add(eMap);
-
+		
 	}
-
+	
 	List searchEvent(String event) {
 		List resultList = null;
-		outter: for (int i = 0; i < eventList.size(); i++) {
-			Map map = (Map) eventList.get(i);
+		outter : for ( int i = 0 ; i < eventList.size() ; i++ ) {
+			Map map = (Map)eventList.get(i);
 			Set set = map.keySet();
 			Iterator ite = set.iterator();
-			while (ite.hasNext()) {
-				String name = (String) ite.next();
-				if (event.equals(name)) {
-					resultList = (List) map.get(name);
+			while ( ite.hasNext() ) {
+				String name = (String)ite.next();
+				if ( event.equals(name) ) {
+					resultList = (List)map.get(name);
 					break outter;
 				}
 			}
 		}
 		return resultList;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
